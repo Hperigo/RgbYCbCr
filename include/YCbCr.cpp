84689b2a18fc298c9e90fdd59fb549136353d55a
Henrique Penteado Kujawski Périgo <hperigo@gmail.com>
2015-06-09 14:12:33 -0300

    First public working version.

--- include/YCbCr.cpp
+++ include/YCbCr.cpp
@@ -7,3 +7,160 @@
 //
 
 #include "YCbCr.h"
+#include "cinder/gl/Texture.h"
+
+using namespace ci;
+
+
+// -------- SurfaceYCbCr
+
+SurfaceYCbCr::SurfaceYCbCr(){
+    
+    
+}
+
+SurfaceYCbCr::~SurfaceYCbCr(){
+    
+    
+}
+
+SurfaceYCbCr::SurfaceYCbCr(const ci::Surface& original, float compressionAmount){
+    
+    setSurface(original, compressionAmount);
+    
+}
+
+void SurfaceYCbCr::setSurface(const ci::Surface &original, float compressionAmount){
+    
+    
+    Y =  Channel(original.getWidth(), original.getHeight());
+    Cb = Channel(original.getWidth() * compressionAmount, original.getHeight() * compressionAmount);
+    Cr = Channel(original.getWidth() * compressionAmount, original.getHeight() * compressionAmount);
+    
+    Surface::ConstIter iter = original.getIter();
+    amt = compressionAmount;
+    
+    while( iter.line() ){
+        while( iter.pixel() ){
+            
+            Vec2i pos = iter.getPos();
+            Vec2i scaledPos = pos * amt;
+            
+            uint8_t r = iter.r();
+            uint8_t g = iter.g();
+            uint8_t b = iter.b();
+            
+            
+            uint8_t y = constrain<uint8_t>( 0.299f * r + 0.587f * g + 0.114f*b, 0, 255 );
+            uint8_t cb = 128 -  ( 0.168736f *r )  - (0.331264f*g) + ( 0.5f * b );
+            uint8_t cr = 128 + (0.5f*r) - (0.418688f*g) - (0.081312f*b);
+            
+            *Y.getData( pos ) = y;
+            *Cb.getData( scaledPos ) = cb;
+            *Cr.getData( scaledPos ) = cr;
+        }
+        
+    }
+    
+    
+    
+}
+
+Surface SurfaceYCbCr::toSurface(){
+    
+    Channel::Iter ch_iter = Y.getIter();
+    
+    Surface8u final(Y.getWidth(), Y.getHeight(), false);
+    
+    
+    while(ch_iter.line()){
+        while(ch_iter.pixel()){
+            
+            auto pos = ch_iter.getPos();
+            Vec2i scaledPos = pos * amt;
+            
+            uint8_t y = ch_iter.v();
+            uint8_t cb = *Cb.getData(scaledPos);
+            uint8_t cr = *Cr.getData(scaledPos);
+            
+            // calculate RGB values
+            uint8_t r = constrain<int16_t>(y + 1.140f * (cr - 128), 0, 255);
+            uint8_t g = constrain<int16_t>(y - 0.34414f * (cb - 128) - 0.71414f * ( cr - 128 ), 0, 255);
+            uint8_t b = constrain<int16_t>(y + 1.772f * (cb - 128), 0, 255);
+            
+            // apply values to final image to pointer p
+            uint8_t *p = final.getData( pos );
+            p[final.getRedOffset()] = r;
+            p[final.getGreenOffset()] = g;
+            p[final.getBlueOffset()] = b;
+        }
+        
+    }
+    
+    return final;
+}
+
+
+//------------ TextureYCbCr
+
+ci::gl::GlslProgRef TextureYCbCr::mShader = nullptr;
+
+TextureYCbCr::TextureYCbCr(const SurfaceYCbCr& surf ){
+    
+    setTexture(surf);
+    
+}
+
+
+void TextureYCbCr::setTexture(const SurfaceYCbCr &surf){
+    
+    YTex = gl::Texture::create( surf.getChannelLuma() );
+    CrTex = gl::Texture::create( surf.getChannelChromaR() );
+    CbTex = gl::Texture::create( surf.getChannelChromaB() );
+    
+    initializeShader();
+}
+
+
+
+void TextureYCbCr::initializeShader(){
+    
+    if(!mShader){
+    
+        try {
+            mShader = gl::GlslProg::create( app::loadAsset("passThru_vert.glsl"), app::loadAsset("YCbCr_frag.glsl") );
+        }
+        catch( gl::GlslProgCompileExc &exc ) {
+            std::cout << "Shader compile error: " << std::endl;
+            std::cout << exc.what();
+        }
+        catch( ... ) {
+            std::cout << "Unable to load shader" << std::endl;
+        }
+    }
+    
+}
+
+void TextureYCbCr::bindShaderTexture(){
+    
+    YTex->bind(0);
+    CrTex->bind(1);
+    CbTex->bind(2);
+    
+    mShader->bind();
+    mShader->uniform( "Y", 0 );  // texunit 0
+    mShader->uniform( "Cr", 1 );  // texunit 1
+    mShader->uniform( "Cb", 2 );  // texunit 1
+    
+}
+
+void TextureYCbCr::unbindShaderTexture(){
+    
+    YTex->disable();
+    CrTex->disable();
+    CbTex->disable();
+    mShader->unbind();
+    
+}
+
+
--- include/YCbCr.h
+++ include/YCbCr.h
@@ -9,6 +9,63 @@
 #ifndef __RGB_YCbCr__YCbCr__
 #define __RGB_YCbCr__YCbCr__
 
-#include <stdio.h>
+#include "cinder/Surface.h"
+#include "cinder/Vector.h"
+#include "cinder/gl/GlslProg.h"
+
+class SurfaceYCbCr{
+public:
+    SurfaceYCbCr();
+    ~SurfaceYCbCr();
+    SurfaceYCbCr(const ci::Surface& original, float compressionAmount = 1.0f);
+    
+    void setSurface(const ci::Surface& original, float compressionAmount = 1.0f);
+    ci::Surface toSurface();
+    
+    ci::Channel getChannelLuma() const { return Y; };
+    ci::Channel getChannelChromaB()const { return Cb; };
+    ci::Channel getChannelChromaR() const { return Cr; };
+    
+    ci::Vec2i getlLumaSize() const { return Y.getSize(); };
+    ci::Vec2i getChromaBlueSize() const { return Cb.getSize(); };
+    ci::Vec2i getChromaRedSize() const { return Cr.getSize(); };
+    
+    float getCompressionAmount() const { return amt; };
+    
+    //returns the number of bytes of all 3 channels combined
+    int getCompressedSize() {return  (Y.getRowBytes() * Y.getHeight()) + (Cb.getRowBytes() * Cb.getHeight()) +  (Cr.getRowBytes() * Cr.getHeight() ); }
+    
+private:
+    
+    ci::Channel Y;
+    ci::Channel Cb;
+    ci::Channel Cr;
+    
+    float amt;
+};
+
+class TextureYCbCr{
+public:
+    TextureYCbCr(){ };
+    ~TextureYCbCr() {  };
+    TextureYCbCr(const SurfaceYCbCr& surf );
+    
+    void bindShaderTexture();
+    void unbindShaderTexture();
+    void setTexture(const SurfaceYCbCr& surf);
+    
+    ci::gl::TextureRef getYTexture() const  { return YTex;  };
+    ci::gl::TextureRef getCbTexture() const { return CbTex; };
+    ci::gl::TextureRef getCrTexture() const { return CrTex; };
+    
+private:
+    static ci::gl::GlslProgRef mShader;
+    
+    void initializeShader();
+    ci::gl::TextureRef YTex;
+    ci::gl::TextureRef CrTex;
+    ci::gl::TextureRef CbTex;
+};
+
 
 #endif /* defined(__RGB_YCbCr__YCbCr__) */
