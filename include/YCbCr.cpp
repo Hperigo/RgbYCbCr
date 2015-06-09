8acc56c5eb67ee3446941b7718905b1ad0e68034
Henrique Penteado Kujawski Périgo <hperigo@gmail.com>
2015-06-09 14:11:44 -0300

    - Added SurfaceYCbCr and TextureYCbCr classes. - Separate files

---
+++ include/YCbCr.cpp
@@ -0,0 +1,9 @@
+//
+//  YCbCr.cpp
+//  RGB_YCbCr
+//
+//  Created by Henrique Penteado Kujawski Périgo on 09/06/15.
+//
+//
+
+#include "YCbCr.h"
---
+++ include/YCbCr.h
@@ -0,0 +1,14 @@
+//
+//  YCbCr.h
+//  RGB_YCbCr
+//
+//  Created by Henrique Penteado Kujawski Périgo on 09/06/15.
+//
+//
+
+#ifndef __RGB_YCbCr__YCbCr__
+#define __RGB_YCbCr__YCbCr__
+
+#include <stdio.h>
+
+#endif /* defined(__RGB_YCbCr__YCbCr__) */
